c8ff4d887b4db1d63f9fb5a5fecf69f90cd93202
kir03 <pehenko.k@icloud.com>
2022-03-22 17:45:15 +0300

    firat commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введи свое имя:");
+string name = Console.ReadLine();
+Console.WriteLine("В каком году ты родился?");
+int year = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сейчас тебе {2022 - year} лет");
